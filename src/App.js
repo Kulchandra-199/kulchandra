0220b4a36b16c8c24a5fc2555311747633051bcd
Kulchandra-199 <kulchandrakandel@gmail.com>
2023-01-10 16:31:40 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './components/home/Home';
 import Skills from './components/Skills/Skills.jsx';
 
 import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer';
+
 import Project from './components/Projects/Project';
 import Qualification1 from './components/qualification/Qualification1.jsx';
 
@@ -20,7 +20,7 @@ const App = () => {
         <Project />
         <Qualification1 />
         <Contact />
-        <Footer />
+
       </main>
     </>
   )
